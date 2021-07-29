83d070d3c9dce6759302f5da85fa2920830c6a13
victoralm <victoralmlinx@gmail.com>
2021-07-29 19:49:18 -0300

    Seção 5: Angular -> Interpolação, Diretiva e Binding

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -24,7 +24,39 @@ export class EventosComponent implements OnInit {
     },
   ];
   */
-  public eventos: any;
+  public eventos: any = [];
+
+  // Property binding
+  widthImgSmall: number = 100;
+  marginImgSmall = 2; // Notar que a atribuição de tipo é opcional
+  showImg: boolean = true;
+  private _listFilter: string = ''; // Requer a importação de FormsModule (que requer "import { FormsModule } from '@angular/forms';") em "app.module.ts"
+  eventosFiltrados: any = [];
+
+  /**
+   * get ListFilter
+   */
+  public get ListFilter(): string {
+    return this._listFilter;
+  }
+  /**
+   * set ListFilter
+   */
+  public set ListFilter(value: string) {
+    this._listFilter = value;
+    this.eventosFiltrados = this._listFilter
+      ? this.FiltrarEventos(this._listFilter)
+      : this.eventos;
+  }
+
+  FiltrarEventos(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { tema: string; local: string }) =>
+        evento.tema.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    );
+  }
 
   constructor(private http: HttpClient) {}
 
@@ -32,12 +64,19 @@ export class EventosComponent implements OnInit {
     this.GetEventos();
   }
 
+  toggleImg() {
+    this.showImg = !this.showImg;
+  }
+
   /**
    * GetEventos
    */
   public GetEventos(): void {
     this.http.get('https://localhost:5001/api/Evento').subscribe(
-      (response) => (this.eventos = response),
+      (response) => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       (error) => console.log(error)
     );
 
